88c1a513074eb51093a38504d175e20a0d1b1582
Lukas <lukas.heyn@gmail.com>
2026-02-01 21:37:11 +0100

    Board Connection

---
+++ test/test_permatracks_data_collection.cpp
@@ -0,0 +1,55 @@
+#include <MagArrayParser.h>
+#include <MagneticFluxDensityData.h>
+#include <MagneticFluxDensityDataRawAK09940A.h>
+#include <MagneticFluxDensityDataRawLIS3MDL.h>
+#include <MagneticFluxDensityDataRawMMC5983MA.h>
+#include <SerialConnection.h>
+
+#include <expected>
+
+class DataPrinterV1 : public virtual SerialConnection, public MiMedMagnetometerArraySerialConnectionBinary<SENSOR_TYPE<MagneticFluxDensityDataRawLIS3MDL, 25, 16>, SENSOR_TYPE<MagneticFluxDensityDataRawMMC5983MA, 0, 25>> {
+	void handle_parse_result(Message<Array<MagneticFluxDensityData, total_mag_sensors>>& magnetic_flux_density_message) override { std::cout << magnetic_flux_density_message << std::endl; }
+
+   public:
+#if defined(_WIN32)
+	std::expected<void, common::Error> connect(std::string port) {
+#elif defined(__APPLE__)
+	std::expected<void, common::Error> connect(std::string port = "/dev/cu.usbserial-0001"){
+#elif defined(__linux__)
+	std::expected<void, common::Error> connect(std::string const& port = "/dev/ttyUSB0") {
+#endif
+		return open_serial_port(port);
+	}
+};
+
+class DataPrinterV2 : public virtual SerialConnection, public MiMedMagnetometerArraySerialConnectionBinary<SENSOR_TYPE<MagneticFluxDensityDataRawAK09940A, 0, 111>> {
+	void handle_parse_result(Message<Array<MagneticFluxDensityData, total_mag_sensors>>& magnetic_flux_density_message) override { std::cout << magnetic_flux_density_message << std::endl; }
+
+   public:
+#if defined(_WIN32)
+	std::expected<void, common::Error> connect(std::string port) {
+#elif defined(__APPLE__)
+	std::expected<void, common::Error> connect(std::string port = "/dev/cu.usbmodem3958386634341"){
+#elif defined(__linux__)
+	std::expected<void, common::Error> connect(std::string const& port = "/dev/ttyACM0") {
+#endif
+		return open_serial_port(port);
+	}
+};
+
+int main() {
+	DataPrinterV2 test;
+
+	if (auto const res = test.connect(); !res.has_value()) return EXIT_FAILURE;
+
+	for (auto i = 0; i < 10000; ++i) {
+		if (auto serial_data = test.read_some(); serial_data.has_value()) {
+			test.parse(serial_data.value());
+		} else {
+			test.close_serial_port();
+			return EXIT_FAILURE;
+		}
+	}
+
+	return EXIT_SUCCESS;
+}
